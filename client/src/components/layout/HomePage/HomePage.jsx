64f80ddbfbd2122fa1cca2df3ca124efff571c0c
subashniraula6@gmail.com <subashniraula6@gmail.com>
2021-05-10 16:36:45 +0545

    fetching of json data from server to application

--- client/src/components/layout/HomePage/HomePage.jsx
+++ client/src/components/layout/HomePage/HomePage.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../../api/api'
 
 const HomePage = () => {
+    const [employee, setEmployee] = useState([]);
+
+    const retrieveEmployees = async () => {
+        const response = await api.get('/employees');
+        if (response.data) console.log(response.data);
+        return response.data;
+    }
+
+    useEffect(() => {
+        const retrieveAllEmployees = async () => {
+            const allEmployees = await retrieveEmployees();
+            setEmployee(allEmployees);
+        }
+        retrieveAllEmployees();
+    }, [])
+
+
     return (
         <div>
             Home page
