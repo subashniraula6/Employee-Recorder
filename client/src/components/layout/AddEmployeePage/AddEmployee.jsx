97318edb6167c88b54b7045391bdbd345d8e8fd0
subashniraula6@gmail.com <subashniraula6@gmail.com>
2021-05-11 13:08:49 +0545

    Add employee form created

--- client/src/components/layout/AddEmployeePage/AddEmployee.jsx
+++ client/src/components/layout/AddEmployeePage/AddEmployee.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
+import {
+    AddEmployeeContainer,
+    EmployeeForm,
+    InputContainer,
+    NameIcon,
+    EmailIcon,
+    Input,
+    PhoneIcon,
+    AddressIcon,
+    CompanyIcon,
+    Button
+} from './AddEmployee.styles'
 
 const AddEmployee = () => {
     return (
-        <div>
-            Add Employee page
-        </div>
+        <AddEmployeeContainer>
+            <h1
+                style={{
+                    'margin-top': '25px',
+                    'color': 'magenta'
+                }}
+            >
+                Add Employee
+             </h1>
+
+            <EmployeeForm>
+                <InputContainer>
+                    <NameIcon />
+                    <Input type='text' placeholder='Name' />
+                </InputContainer>
+
+                <InputContainer>
+                    <EmailIcon />
+                    <Input type='email' placeholder='Email' />
+                </InputContainer>
+
+                <InputContainer>
+                    <PhoneIcon />
+                    <Input type='text' placeholder='Phone' />
+                </InputContainer>
+
+                <InputContainer>
+                    <AddressIcon />
+                    <Input type='text' placeholder='Address' />
+                </InputContainer>
+
+                <InputContainer>
+                    <CompanyIcon />
+                    <Input type='text' placeholder='Company' />
+                </InputContainer>
+
+                <InputContainer>
+                    <Button> Submit </Button>
+                </InputContainer>
+
+            </EmployeeForm>
+        </AddEmployeeContainer>
     )
 }
 
--- client/src/redux/selectors/employeeSelector.js
+++ client/src/redux/selectors/employeeSelector.js
@@ -2,6 +2,7 @@ import {createSelector} from 'reselect'
 
 export const employeeReducerSelector = state => state.employee
 
+//select employees array
 export const employeesSelector = createSelector(
     [employeeReducerSelector],
     employee => employee.employees
